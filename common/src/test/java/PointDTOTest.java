20409dce90905b2bfc34f6412bca0324fd940886
sobolkindm <namora000@yandex.ru>
2022-04-23 07:51:48 +0300

    JSON -> String -> JSON

---
+++ common/src/test/java/PointDTOTest.java
@@ -0,0 +1,30 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1650688943262}";
+    private String autoId = "o567gfd";
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
+    }
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, dto.getAutoId());
+        assertEquals(1650688943262L, dto.getTime());
+    }
+
+}
