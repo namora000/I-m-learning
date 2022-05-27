bce46cf530572709898ef77ada5a65ba4ec1dc7f
sobolkindm <namora000@yandex.ru>
2022-05-27 08:52:33 +0300

    Автоматизация тестирования -> GpsPoint+PointTest

--- tracker-core/src/test/java/jdev/dto/PointTest.java
+++ tracker-core/src/test/java/jdev/dto/PointTest.java
@@ -1,6 +1,7 @@
 package jdev.dto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jdev.dto.services.GpsPoint;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,26 +9,36 @@ import static org.junit.Assert.assertTrue;
 
 public class PointTest {
 
-    private final String autoId = "o567gfd";
-    private String json = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1650688943262}";
+    private String lat = "38.760888";
+    private String lon = "44.670992";
+    private String alt = "97.0";
+    private String speed = "40";
+    private String time = Long.toString(System.currentTimeMillis());
+    private String json = "{\"latitude\":\""+lat+"\",\"longitude\":\""+lon+"\",\"altitude\":\""+alt+"\",\"speed\":\""+speed+"\",\"time\":\""+time+"\"}";
 
 
     @Test
     public void encodeDto() throws Exception {
-        Point point = new Point();
-        point.setLat(56);
-        point.setLon(74);
-        point.setAutoId(autoId);
-        point.setTime(System.currentTimeMillis());
-        assertTrue(point.toJson().contains("\"lat\":56"));
-        assertTrue(point.toJson().contains("\"time\":"));
+        GpsPoint point = new GpsPoint();
+        point.setLatitude(lat);
+        point.setLongitude(lon);
+        point.setAltitude(alt);
+        point.setSpeed(speed);
+        point.setTime(time);
+        assertTrue(point.toJson().contains(lat));
+        assertTrue(point.toJson().contains(lon));
+        assertTrue(point.toJson().contains(alt));
+        assertTrue(point.toJson().contains(speed));
+        assertTrue(point.toJson().contains(time));
+        assertEquals(json, point.toJson());
+        assertEquals(json, point.toString());
         System.out.println(point.toJson());
+        System.out.println(point.toString());
     }
+    @Test
     public void decodeDto() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        Point dto = mapper.readValue(json, Point.class);
-        assertEquals(autoId, dto.getAutoId());
-        assertEquals(1650688943262L, dto.getTime());
+        GpsPoint dto = mapper.readValue(json, GpsPoint.class);
     }
 
 }
